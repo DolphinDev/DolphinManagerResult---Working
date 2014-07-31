98ff3ff3e0d1d388c9cf9998e17e960bf400a105
DolphinDev <miguel.dxm@gmail.com>
2014-07-31 10:42:45 +0100

    Update Startup.java

--- src/net/DolphinDev/DMR/Startup.java
+++ src/net/DolphinDev/DMR/Startup.java
@@ -1,1 +1,37 @@
-//TODO: Add Code
+package net.DolphinDev.DMR;
+
+
+import net.DolphinDev.DMR.Events.ResultManager;
+import net.DolphinDev.StringAPI.MessageManager;
+
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class Startup extends JavaPlugin {
+	
+	public void onEnable() {
+		System.out.println(ChatColor.GREEN + "[DolphinManagerResult] Website: " + MessageManager.getDonationLink());
+	     execute();
+		//Why not green?
+		//Testing ;P it might be Color.GREEN instead 
+	}
+
+	
+	
+	private void execute() {
+		getServer().getPluginManager().registerEvents(new ResultManager(), this);
+
+		
+	}
+
+
+
+	@Override
+	public void onDisable() {
+         System.out.println("CHECK OUT THE WEBSITE PLS!");
+	}
+	
+	
+	
+}
